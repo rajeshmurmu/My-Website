1622f28c77328332f822fcf183b51fc90272f243
Rajesh Murmu <rajeshsonmurmur2017@gmail.com>
2024-05-25 02:52:12 +0530

    remove bg

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -96,7 +96,7 @@ export default function page() {
     <>
       {/* <!-- component --> */}
       {/* <div className="h-full bg-gradient-to-tl from-green-400 to-indigo-900 w-full py-16 px-4"> */}
-      <div className="relative inset-0 h-full w-full items-center px-5 py-10 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <div className="relative inset-0 h-full w-full items-center px-5 py-10">
         <div className="flex flex-col items-center justify-center">
           <div className="bg-white shadow rounded lg:w-1/3  md:w-1/2 w-full p-10 mt-16">
             <p
